308b401acbbc033bb9fee3f6d3e5cd3e0fa3353e
Dan Strong <danielstrong@mac.com>
2018-01-04 15:27:37 -0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-        var operation = $( "#operation option:selected" ).text();
-        var num1 = $( "#num1" ).val();
-        var num2 = $( "#num2").val();
-        var answer = NaN;
-        
-        $("button").click(function(){
+var answerDiv = document.querySelector('#answer');
+var operation = $( "#operation option:selected" ).text();
+var num1 = $( "#num1" ).val();
+var num2 = $( "#num2").val();
+var answer = NaN;
+
+function calculateNumber() {
             if (num2 === 0 && operation === 'divide') {
                 alert('Cannot divide by zero');
             }
@@ -17,12 +17,12 @@ $(document).ready(function(){
             } else if (operation === 'divide') {
                 answer = divideNumbers(num1, num2);  
             }
-            
-            $('#answer').html('<h3>' + answer + '</h3>'); 
-        });
-        
-        function addNumbers(num1, num2) {
-            return num1 + num2;
+   
+            answerDiv.innerHTML = '<h3>' + answer + </h3>;
+ };
+                          
+function addNumbers(num1, num2) {
+    return num1 + num2;
         }
 
         function subtractNumbers(num1, num2) {
@@ -37,4 +37,10 @@ $(document).ready(function(){
             return num1 / num2;
         }
 
-});
+
+                          
+                          
+                          
+                         
+        
+        
